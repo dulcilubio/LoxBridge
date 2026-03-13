9214a9ca1f95067f690201bbffb43a7413083141
Erik <erik@frick.nu>
2026-03-13 17:03:30 +0100

    Initial Commit

---
+++ LoxBrige/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoxBrige
+//
+//  Created by Erik Frick on 2026-03-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoxBrige/LoxBrigeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoxBrigeApp.swift
+//  LoxBrige
+//
+//  Created by Erik Frick on 2026-03-13.
+//
+
+import SwiftUI
+
+@main
+struct LoxBrigeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
